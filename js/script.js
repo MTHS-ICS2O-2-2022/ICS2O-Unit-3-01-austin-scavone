262b37c4427db85b7afbd1aed21eb3ef472a0223
austin <124389297+austin-scavone@users.noreply.github.com>
2023-04-12 20:29:39 +0000

    work

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+'use strict'
+
+/**
+ * This function is calculates the area of a trapezoid
+*/
+'use strict'
+
+
+function calculate () {
+// input
+const a_base= parseFloat(document.getElementById('a-base').value)
+const b_base= parseFloat(document.getElementById('b-base').value)
+const height= parseFloat(document.getElementById('height').value)
+
+// process
+
+const area= (a_base + b_base) / 2 * height
+
+// output
+document.getElementById('area').innerHTML = 'Area is: '+ area +' cm²'
+}
